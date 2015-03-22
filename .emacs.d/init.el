57fb83468d27066fad2884f27a0824bdfb7775cf
Jordon Smith <jrd@mockra.net>
2015-03-22 20:28:18 +0000

    Emacs, Powerline + JavaScript

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -7,7 +7,8 @@
 
 ;; List of used packages
 (setq package-list '(evil evil-leader linum-relative fill-column-indicator
-                          key-chord neotree elixir-mode alchemist))
+                          key-chord neotree elixir-mode alchemist powerline
+                          powerline-evil flx-ido js2-mode jsx-mode))
 
 ;; Update package archive
 (unless package-archive-contents
@@ -21,11 +22,13 @@
 ;; Remove trailing whitespace
 (add-hook 'before-save-hook 'delete-trailing-whitespace)
 
+
 ;; Relative line number
 (require `linum-relative)
 (setq linum-relative-format "%2s ")
 (setq linum-relative-current-symbol "") ; Current number
 (global-linum-mode 1)
+(column-number-mode t)
 
 ;; No tabs
 (setq-default indent-tabs-mode nil)
@@ -36,13 +39,21 @@
 ;; 80-char rule
 (require 'fill-column-indicator)
 (setq-default fill-column 80)
-(fci-mode 1)
+(add-hook 'after-change-major-mode-hook 'fci-mode)
 
 ;; No #backup-files#
 (setq make-backup-files nil)
 
 ;; NeoTree
 (require 'neotree)
+
+;; ido
+(require 'flx-ido)
+(ido-mode 1)
+(ido-everywhere 1)
+(flx-ido-mode 1)
+(setq ido-enable-flex-matching t) ; Disable ido faces to see flx highlights.
+(setq ido-use-faces nil)
 
 ;; ---------------------------------
 ;; EVIL
@@ -58,3 +69,13 @@
 (key-chord-define evil-insert-state-map  "jk" 'evil-normal-state)
 
 (evil-leader/set-key "d" 'neotree-toggle)
+
+;; Powerline
+(require 'powerline)
+(require 'powerline-evil)
+(powerline-evil-vim-color-theme)
+
+;; JavaScript
+(add-to-list 'auto-mode-alist '("\\.js\\'" . js2-mode))
+(add-to-list 'auto-mode-alist '("\\.jsx\\'" . jsx-mode))
+(autoload 'jsx-mode "jsx-mode" "JSX mode" t)
